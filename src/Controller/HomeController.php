1a308ebebbb0c3347f3fdb2c19a6ad70f80ad4fa
Bruno Ramalho <bramalho@divante.pl>
2020-10-12 19:53:02 +0200

    home controller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->json([
+            'message' => 'It works!',
+            'api' => '/api/v1',
+            'docs' => '/docs'
+        ]);
+    }
+}
